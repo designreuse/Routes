9676b88e27560d838e61edc2eb51fa10d214e237
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 19:07:48 +0900

    Defined empty object as a constant

--- routes-server/src/main/java/com/t28/routes/entity/itinerary/Constraint.java
+++ routes-server/src/main/java/com/t28/routes/entity/itinerary/Constraint.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.t28.routes.entity.Entity;
 
 public class Constraint extends Entity {
+    public static final Constraint EMPTY = new Constraint();
+
     @JsonProperty
     public int stayingTime;
 
@@ -12,4 +14,8 @@ public class Constraint extends Entity {
 
     @JsonProperty
     public int departTime;
+
+    public boolean isEmpty() {
+        return stayingTime == 0 && arriveTime == 0 && departTime == 0;
+    }
 }
