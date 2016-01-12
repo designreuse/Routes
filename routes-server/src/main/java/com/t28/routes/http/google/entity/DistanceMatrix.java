d350b85c5e48087383a21f23cd60335e11e0c727
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 20:36:01 +0900

    Fixed type of List because Jackson could not create List instance

--- routes-server/src/main/java/com/t28/routes/http/google/entity/DistanceMatrix.java
+++ routes-server/src/main/java/com/t28/routes/http/google/entity/DistanceMatrix.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sun.tools.javac.util.List;
 
+import java.util.ArrayList;
+
 /**
  * @link https://developers.google.com/maps/documentation/distance-matrix
  */
@@ -15,13 +17,13 @@ public class DistanceMatrix {
     private String status;
 
     @JsonProperty(value = "destination_addresses", required = true)
-    private List<String> destinationAddresses;
+    private ArrayList<String> destinationAddresses;
 
     @JsonProperty(value = "origin_addresses", required = true)
-    private List<String> originAddresses;
+    private ArrayList<String> originAddresses;
 
     @JsonProperty(required = true)
-    private List<Elements> rows;
+    private ArrayList<Elements> rows;
 
     public boolean isSuccess() {
         return STATUS_SUCCESS.equals(status);
