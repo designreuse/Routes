0d6c5f7ed21f334d07bddc9e5669b4c9e58c2bc7
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 11:23:29 +0900

    Implemented application class to run

--- routes-server/src/main/java/com/t28/route/RouteApplication.java
+++ routes-server/src/main/java/com/t28/route/RouteApplication.java
@@ -1,15 +1,25 @@
 package com.t28.route;
 
+import com.t28.route.resource.ItineraryResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 
+import javax.print.attribute.standard.MediaSize;
+
 public class RouteApplication extends Application<RouteConfiguration> {
+    private static final String NAME = "Routes";
+
     public static void main(String[] args) throws Exception {
         new RouteApplication().run(args);
     }
 
     @Override
-    public void run(RouteConfiguration configuration, Environment environment) throws Exception {
+    public String getName() {
+        return NAME;
+    }
 
+    @Override
+    public void run(RouteConfiguration configuration, Environment environment) throws Exception {
+        environment.jersey().register(new ItineraryResource(null));
     }
 }
