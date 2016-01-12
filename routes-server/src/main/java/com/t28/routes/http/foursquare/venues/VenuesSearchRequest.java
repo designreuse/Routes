86bcfa8320de1c05ab58e63b5d1d818f3fc14048
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 16:43:24 +0900

    Added a method that can specify category and changed a method interface to give limit as integer

--- routes-server/src/main/java/com/t28/routes/http/foursquare/venues/VenuesSearchRequest.java
+++ routes-server/src/main/java/com/t28/routes/http/foursquare/venues/VenuesSearchRequest.java
@@ -9,6 +9,7 @@ import com.t28.routes.http.foursquare.FoursquareRequest;
 public class VenuesSearchRequest extends FoursquareRequest<VenuesSearch> {
     private static final String URL = "https://api.foursquare.com/v2/venues/search";
     private static final String KEY_QUERY = "query";
+    private static final String KEY_CATEGORY_ID = "categoryId";
     private static final String KEY_COORDINATE = "ll";
     private static final String KEY_RADIUS = "radius";
     private static final String KEY_LIMIT = "limit";
@@ -36,6 +37,10 @@ public class VenuesSearchRequest extends FoursquareRequest<VenuesSearch> {
         return (VenuesSearchRequest) query(KEY_QUERY, query);
     }
 
+    public VenuesSearchRequest category(String category) {
+        return (VenuesSearchRequest) query(KEY_CATEGORY_ID, category);
+    }
+
     public VenuesSearchRequest coordinate(double lat, double lon) {
         return (VenuesSearchRequest) query(KEY_COORDINATE, String.format("%f,%f", lat, lon));
     }
@@ -44,7 +49,7 @@ public class VenuesSearchRequest extends FoursquareRequest<VenuesSearch> {
         return (VenuesSearchRequest) query(KEY_RADIUS, String.valueOf(radius));
     }
 
-    public VenuesSearchRequest limit(String limit) {
+    public VenuesSearchRequest limit(int limit) {
         return (VenuesSearchRequest) query(KEY_LIMIT, limit);
     }
 }
