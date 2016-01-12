a8c0129874d4c4173a404a8d30c02c6bf7d010e7
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 11:18:26 +0900

    Added a method named main to launch the application

--- routes-server/src/main/java/com/t28/route/RouteApplication.java
+++ routes-server/src/main/java/com/t28/route/RouteApplication.java
@@ -4,6 +4,10 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 
 public class RouteApplication extends Application<RouteConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new RouteApplication().run(args);
+    }
+
     @Override
     public void run(RouteConfiguration configuration, Environment environment) throws Exception {
 
