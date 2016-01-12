e4c446a22ef3461c5590bbcfa25af8fe962a3084
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 18:00:51 +0900

    Defined http header keys as constants

---
+++ routes-server/src/main/java/com/t28/routes/http/Headers.java
@@ -0,0 +1,7 @@
+package com.t28.routes.http;
+
+public class Headers {
+    public static final String USER_AGENT = "User-Agent";
+    public static final String ACCEPT_ENCODING = "Accept-Encoding";
+    public static final String ACCEPT_LANGUAGE = "Accept-Language";
+}
