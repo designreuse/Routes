8be9cf0ba64515d14fc1eccc05ce1b8b7396a40a
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 10:47:01 +0900

    Initial commit

---
+++ route-server/src/main/java/com/t28/route/RouteApplication.java
@@ -0,0 +1,11 @@
+package com.t28.route;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class RouteApplication extends Application<RouteConfiguration> {
+    @Override
+    public void run(RouteConfiguration configuration, Environment environment) throws Exception {
+
+    }
+}
---
+++ route-server/src/main/java/com/t28/route/RouteConfiguration.java
@@ -0,0 +1,6 @@
+package com.t28.route;
+
+import io.dropwizard.Configuration;
+
+public class RouteConfiguration extends Configuration {
+}
