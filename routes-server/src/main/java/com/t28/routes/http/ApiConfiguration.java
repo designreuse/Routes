8b2e651265a4ccb5e291984537eef1a13e390a6d
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 20:36:28 +0900

    Added the GoogleFactory into ApiConfiguration

--- routes-server/src/main/java/com/t28/routes/http/ApiConfiguration.java
+++ routes-server/src/main/java/com/t28/routes/http/ApiConfiguration.java
@@ -2,6 +2,7 @@ package com.t28.routes.http;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.t28.routes.http.foursquare.FoursquareFactory;
+import com.t28.routes.http.google.GoogleFactory;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -9,8 +10,17 @@ import javax.validation.constraints.NotNull;
 public class ApiConfiguration {
     @Valid
     @NotNull
+    @JsonProperty("google")
+    private GoogleFactory googleFactory;
+
+    @Valid
+    @NotNull
     @JsonProperty("foursquare")
     private FoursquareFactory foursquareFactory;
+
+    public GoogleFactory getGoogleFactory() {
+        return googleFactory;
+    }
 
     public FoursquareFactory getFoursquareFactory() {
         return foursquareFactory;
