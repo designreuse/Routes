8d2156e2c184d3cb9c41d9db646981d51f388905
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 17:19:47 +0900

    Added base class that represents entity

---
+++ routes-server/src/main/java/com/t28/routes/entity/Entity.java
@@ -0,0 +1,20 @@
+package com.t28.routes.entity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public abstract class Entity {
+    private final ObjectMapper objectMapper;
+
+    protected Entity(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public String toJson() {
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new JsonConvertException(e);
+        }
+    }
+}
---
+++ routes-server/src/main/java/com/t28/routes/entity/JsonConvertException.java
@@ -0,0 +1,7 @@
+package com.t28.routes.entity;
+
+public class JsonConvertException extends RuntimeException {
+    public JsonConvertException(Throwable cause) {
+        super(cause);
+    }
+}
