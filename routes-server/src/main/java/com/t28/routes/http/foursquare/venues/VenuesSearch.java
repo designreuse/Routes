4a151efba8fe01038012dcdadc10f9aab43687b0
Tatsuya Maki <tatsuya.maki@skyscanner.jp>
2016-01-12 15:40:36 +0900

    Added getter methods to access private properties

--- routes-server/src/main/java/com/t28/routes/http/foursquare/venues/VenuesSearch.java
+++ routes-server/src/main/java/com/t28/routes/http/foursquare/venues/VenuesSearch.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.t28.routes.http.foursquare.entity.Meta;
 import com.t28.routes.http.foursquare.entity.TinyVenue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -16,12 +17,35 @@ public class VenuesSearch {
     @JsonProperty(required = true)
     private Response response;
 
+    public Meta getMeta() {
+        return meta;
+    }
+
+    public Response getResponse() {
+        return response;
+    }
+
     public static class Response {
         @JsonProperty
         private List<TinyVenue> venues;
 
         @JsonProperty(value = "confident")
         private boolean isConfident;
+
+        public List<TinyVenue> getVenues() {
+            return new ArrayList<TinyVenue>(venues);
+        }
+
+        public boolean isConfident() {
+            return isConfident;
+        }
+
+        public int count() {
+            if (!hasVenues()) {
+                return 0;
+            }
+            return venues.size();
+        }
 
         public boolean hasVenues() {
             return venues != null;
